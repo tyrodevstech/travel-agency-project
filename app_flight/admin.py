0c277ba98a2d465b4bf7b8d648089c546c3bc1ad
GregoryAyon <royayon229@gmail.com>
2023-06-27 22:37:30 +0600

    Flight-Models-0.1

--- app_flight/admin.py
+++ app_flight/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from app_flight.models import AirportModel
+from app_flight.models import *
 
 # Register your models here.
 
 admin.site.register(AirportModel)
+admin.site.register(AirplaneModel)
+admin.site.register(DiscountModel)
+admin.site.register(AirPlaneTicketModel)
+admin.site.register(OrderFlightModel)
