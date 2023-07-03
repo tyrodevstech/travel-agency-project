0a575e77dbb3d41c746eed5d739af96d366db058
sanaullahrabbi <srrabbi01@gmail.com>
2023-07-03 20:42:55 +0600

    some template design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./static/**/*.{html,js}", "./templates/**/*.{html,js}"],
+  content: [
+    "./static/**/*.{html,js}", 
+    "./templates/**/*.{html,js}",
+    "./node_modules/flowbite/**/*.js",
+  ],
   theme: {
+    container: {
+      center: true,
+    },
     extend: {},
   },
   plugins: [
-    require("daisyui")
+    require("daisyui"),
+    require('flowbite/plugin')
   ],
 }
 
